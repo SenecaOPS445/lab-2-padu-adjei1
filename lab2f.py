332869151c608d9f99fb478b51e6b9c83cbd5638
Padu-adjei1 <padu-adjei1@myseneca.ca>
2024-05-22 15:14:32 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Prince Adu-Adjei
+# Author ID: 112407226
+# Date Created: 2024/05/22
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
